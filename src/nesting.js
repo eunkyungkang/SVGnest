a14f9d8d884dc70d14d547d34308f4bf1e91eefe
Henry Boisgibault <h.boisgibault@gmail.com>
2023-01-21 16:32:14 +0100

    Removed progress file

--- src/nesting.js
+++ src/nesting.js
@@ -3,14 +3,58 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var start = document.getElementById('start');
     var startlabel = document.getElementById('startlabel');
 
+    // config
+    var configvisible = false;
     var config = document.getElementById('config');
     var configbutton = document.getElementById('configbutton');
+    var configsave = document.getElementById('configsave');
 
     var isworking = false;
     var iterations = 0;
 
     var prevpercent = 0;
     var startTime = null;
+
+    configbutton.onclick = function(){
+        if(this.className == 'button config disabled'){
+            return false;
+        }
+        if(!configvisible){
+            config.className = 'active';
+            configbutton.className = 'button close';
+        }
+        else{
+            config.className = '';
+            configbutton.className = 'button config';
+        }
+        configvisible = !configvisible;
+        
+        return false;
+    }
+
+    configsave.onclick = function(){
+        var c = {};
+        var inputs = document.querySelectorAll('#config input');
+        for(var i=0; i<inputs.length; i++){
+            var key = inputs[i].getAttribute('data-config');
+            if(inputs[i].getAttribute('type') == 'text'){
+                c[key] = inputs[i].value;
+            }
+            else if(inputs[i].getAttribute('type') == 'checkbox'){
+                c[key] = inputs[i].checked;
+            }
+        }
+        
+        window.SvgNest.config(c);
+        
+        // new configs will invalidate current nest
+        if(isworking){
+            stopnest();
+        }
+        configvisible = false;
+        config.className = '';
+        return false;
+    }
 
     const startProgress = (percent) => {
         var transition = percent > prevpercent ? '; transition: width 0.1s' : '';

3cfb7d6d5b511407d5decaf2e1e078f67cf73325
Henry Boisgibault <h.boisgibault@gmail.com>
2023-01-21 16:21:14 +0100

    Removed webpack

--- src/download.js
+++ src/download.js
@@ -1,57 +1,56 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    var download = document.getElementById('download');
+    var display = document.getElementById('select');
 
-var FileSaver = require('file-saver');
-
-var download = document.getElementById('download');
-var display = document.getElementById('select');
-
-download.onclick = function() {
-    if(download.className == 'button download disabled'){
-        return false;
-    }
-    
-    var bins = document.getElementById('bins');
-    
-    if(bins.children.length == 0){
-        message.innerHTML = 'No SVG to export';
-        message.className = 'error animated bounce';
-        return
-    }
-    
-    var svg;
-    svg = display.querySelector('svg');
-    
-    if(!svg){
-        svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    }
-    
-    svg = svg.cloneNode(false);
-    
-    // maintain stroke, fill etc of input
-    if(SvgNest.style){
-        svg.appendChild(SvgNest.style);
-    }
-    
-    var binHeight = parseInt(bins.children[0].getAttribute('height'));
-    
-    for(var i=0; i<bins.children.length; i++){
-        var b = bins.children[i];
-        var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-        group.setAttribute('transform', 'translate(0 '+binHeight*1.1*i+')');
-        for(var j=0; j<b.children.length; j++){
-            group.appendChild(b.children[j].cloneNode(true));
+    download.onclick = function() {
+        if(download.className == 'button download disabled'){
+            return false;
         }
         
-        svg.appendChild(group);
+        var bins = document.getElementById('bins');
+        
+        if(bins.children.length == 0){
+            message.innerHTML = 'No SVG to export';
+            message.className = 'error animated bounce';
+            return
+        }
+        
+        var svg;
+        svg = display.querySelector('svg');
+        
+        if(!svg){
+            svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        }
+        
+        svg = svg.cloneNode(false);
+        
+        // maintain stroke, fill etc of input
+        if(SvgNest.style){
+            svg.appendChild(SvgNest.style);
+        }
+        
+        var binHeight = parseInt(bins.children[0].getAttribute('height'));
+        
+        for(var i=0; i<bins.children.length; i++){
+            var b = bins.children[i];
+            var group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+            group.setAttribute('transform', 'translate(0 '+binHeight*1.1*i+')');
+            for(var j=0; j<b.children.length; j++){
+                group.appendChild(b.children[j].cloneNode(true));
+            }
+            
+            svg.appendChild(group);
+        }
+        
+        var output;
+        if(typeof XMLSerializer != 'undefined'){
+            output = (new XMLSerializer()).serializeToString(svg);
+        }
+        else{
+            output = svg.outerHTML;
+        }
+        
+        var blob = new Blob([output], {type: "image/svg+xml;charset=utf-8"});
+        saveAs(blob, "SVGnest-output.svg");
     }
-    
-    var output;
-    if(typeof XMLSerializer != 'undefined'){
-        output = (new XMLSerializer()).serializeToString(svg);
-    }
-    else{
-        output = svg.outerHTML;
-    }
-    
-    var blob = new Blob([output], {type: "image/svg+xml;charset=utf-8"});
-    FileSaver.saveAs(blob, "SVGnest-output.svg");
-}
+})
--- src/toolbar.js
+++ src/toolbar.js
@@ -1,36 +1,35 @@
-    
-require('./config');
+document.addEventListener('DOMContentLoaded', (event) => {
+    var zoomlevel = 1.0;
+    var zoomin = document.getElementById('zoominbutton');
+    var zoomout = document.getElementById('zoomoutbutton');
+    var exit = document.getElementById('exitbutton');
 
-var zoomlevel = 1.0;
-var zoomin = document.getElementById('zoominbutton');
-var zoomout = document.getElementById('zoomoutbutton');
-var exit = document.getElementById('exitbutton');
+    zoomin.onclick = function() {
+        if(this.className == 'button zoomin disabled') {
+            return false;
+        }
+        zoomlevel *= 1.2;
+        var svg = document.querySelector('#select svg');
+        if(svg) {
+            svg.setAttribute('style', 'transform-origin: top left; transform:scale('+zoomlevel+'); -webkit-transform:scale('+zoomlevel+'); -moz-transform:scale('+zoomlevel+'); -ms-transform:scale('+zoomlevel+'); -o-transform:scale('+zoomlevel+');');
+        }
+    }
 
-zoomin.onclick = function() {
-    if(this.className == 'button zoomin disabled') {
-        return false;
+    zoomout.onclick = function(){
+        if(this.className == 'button zoomout disabled') {
+            return false;
+        }
+        zoomlevel *= 0.8;
+        if(zoomlevel < 0.02) {
+            zoomlevel = 0.02;
+        }
+        var svg = document.querySelector('#select svg');
+        if(svg) {
+            svg.setAttribute('style', 'transform-origin: top left; transform:scale('+zoomlevel+'); -webkit-transform:scale('+zoomlevel+'); -moz-transform:scale('+zoomlevel+'); -ms-transform:scale('+zoomlevel+'); -o-transform:scale('+zoomlevel+');');
+        }
     }
-    zoomlevel *= 1.2;
-    var svg = document.querySelector('#select svg');
-    if(svg) {
-        svg.setAttribute('style', 'transform-origin: top left; transform:scale('+zoomlevel+'); -webkit-transform:scale('+zoomlevel+'); -moz-transform:scale('+zoomlevel+'); -ms-transform:scale('+zoomlevel+'); -o-transform:scale('+zoomlevel+');');
-    }
-}
 
-zoomout.onclick = function(){
-    if(this.className == 'button zoomout disabled') {
-        return false;
+    exit.onclick = function() {
+        location.reload();
     }
-    zoomlevel *= 0.8;
-    if(zoomlevel < 0.02) {
-        zoomlevel = 0.02;
-    }
-    var svg = document.querySelector('#select svg');
-    if(svg) {
-        svg.setAttribute('style', 'transform-origin: top left; transform:scale('+zoomlevel+'); -webkit-transform:scale('+zoomlevel+'); -moz-transform:scale('+zoomlevel+'); -ms-transform:scale('+zoomlevel+'); -o-transform:scale('+zoomlevel+');');
-    }
-}
-
-exit.onclick = function() {
-    location.reload();
-}
+})

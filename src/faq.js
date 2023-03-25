7c7418842ae9c68d606a73c4dad05b2af38b7035
eunkyungkang <60553555+eunkyungkang@users.noreply.github.com>
2023-03-25 17:13:46 +0100

    pricing toggle

--- src/faq.js
+++ src/faq.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
         faqvisible = !faqvisible;
     };
 })
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Pricing toggle
+    var pricing = document.getElementById('pricing');
+    var pricingbutton = document.getElementById('pricingbutton');
+
+    var pricingvisible = false;
+    pricingbutton.onclick = function(e){
+        if(!pricingvisible){
+            pricing.setAttribute('style','display: flex');
+            pricing.scrollIntoView({ behavior: "smooth" });
+        }
+        else{
+            pricing.setAttribute('style', 'display: none');
+        }
+        pricingvisible = !pricingvisible;
+    };
+})
